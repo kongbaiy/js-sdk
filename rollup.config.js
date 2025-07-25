e9b1d212c3e4e2d660a1cc6daf7ae62f1a9ae3f0
kl <kanglu-email@qq.com>
2025-07-25 17:57:55 +0800

    feat: 配置更新

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,52 @@
-import typescript from '@rollup/plugin-typescript';
+// import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 's', // 浏览器中全局变量名
-    },
-    {
-      file: 'dist/index.esm.js',
+// export default {
+//   input: 'src/index.ts',
+//   output: [
+//     {
+//       file: 'dist/index.umd.js',
+//       format: 'umd',
+//       name: 's', // 浏览器中全局变量名
+//     },
+//     {
+//       file: 'dist/index.esm.js',
+//       format: 'es'
+//     }
+//   ],
+//   plugins: [
+//     typescript({
+//       tsconfig: './tsconfig.json',
+//       declaration: true,
+//       declarationDir: 'dist/types',
+//       rootDir: 'src',
+//     }),
+//   ],
+// };
+
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  // 第一轮：打包 JS
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'esm'
+      }
+    ],
+    plugins: [typescript()]
+  },
+
+  // 第二轮：打包类型
+  {
+    input: 'src/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
       format: 'es'
-    }
-  ],
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist/types',
-      rootDir: 'src',
-    }),
-  ],
-};
+    },
+    plugins: [dts()]
+  }
+];
+
