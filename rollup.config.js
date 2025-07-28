df0b21223f38c2819b0c20220c8d7b400bf0acf5
kl <kanglu-email@qq.com>
2025-07-28 10:03:37 +0800

    feat: update

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,3 @@
-// import typescript from '@rollup/plugin-typescript';
-
-// export default {
-//   input: 'src/index.ts',
-//   output: [
-//     {
-//       file: 'dist/index.umd.js',
-//       format: 'umd',
-//       name: 's', // 浏览器中全局变量名
-//     },
-//     {
-//       file: 'dist/index.esm.js',
-//       format: 'es'
-//     }
-//   ],
-//   plugins: [
-//     typescript({
-//       tsconfig: './tsconfig.json',
-//       declaration: true,
-//       declarationDir: 'dist/types',
-//       rootDir: 'src',
-//     }),
-//   ],
-// };
-
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
