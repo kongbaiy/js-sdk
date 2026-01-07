f88363f3de195e9ffdab7d8b23d4b848118d5d41
kebi <kanglu-email@qq.com>
2026-01-08 00:24:56 +0800

    feat: 格式话(ws)

--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -1,123 +1,128 @@
-type WebSocketState = 'CONNECTING' | 'OPEN' | 'CLOSING' | 'CLOSED'
+type WebSocketState = "CONNECTING" | "OPEN" | "CLOSING" | "CLOSED";
 
 interface WebSocketConfig {
-  heartbeat?: boolean
-  heartbeatTime?: number
-  reconnect: boolean
-  reconnectTime?: number
-  maxReconnectNumber?: number
+	heartbeat?: boolean;
+	heartbeatTime?: number;
+	reconnect: boolean;
+	reconnectTime?: number;
+	maxReconnectNumber?: number;
 }
 
 const defaultWebSocketConfig: WebSocketConfig = {
-  heartbeat: false,
-  heartbeatTime: 15000,
-  reconnect: false,
-  reconnectTime: 1000,
-  maxReconnectNumber: 3,
-}
+	heartbeat: false,
+	heartbeatTime: 15000,
+	reconnect: false,
+	reconnectTime: 1000,
+	maxReconnectNumber: 3,
+};
 
-export function createWebSocket(url: string, config: WebSocketConfig | null, onOpen?: Function | null): WebSocket {
-  config = { ...defaultWebSocketConfig, ...config }
-  let open: Function | null = reconnectWebSocket()
+export function createWebSocket(
+	url: string,
+	config: WebSocketConfig | null,
+	onOpen?: Function | null
+): WebSocket {
+	config = { ...defaultWebSocketConfig, ...config };
+	let open: Function | null = reconnectWebSocket();
 
-  const init = (): WebSocket => {
-    const ws = new WebSocket(url);
+	const init = (): WebSocket => {
+		const ws = new WebSocket(url);
 
-    ws.onopen = () => {
-      if (config?.heartbeat) heartbeat(ws, config);
+		ws.onopen = () => {
+			if (config?.heartbeat) heartbeat(ws, config);
 
-      onOpen?.()
-    }
+			onOpen?.();
+		};
 
-    ws.onerror = () => {
-      if (config?.reconnect && open) {
-        open(ws, config, init)
-      }
-      else {
-        open = null
-        config = null
-      }
-    }
+		ws.onerror = () => {
+			if (config?.reconnect && open) {
+				open(ws, config, init);
+			} else {
+				open = null;
+				config = null;
+			}
+		};
 
-    return ws
-  }
+		return ws;
+	};
 
-  return init()
+	return init();
 }
 
 export function getState(ws: WebSocket, type: WebSocketState): boolean {
-  return ws.readyState === WebSocket[type]
+	return ws.readyState === WebSocket[type];
 }
 
 function reconnectWebSocket() {
-  let reconnectTimeout: NodeJS.Timeout | null
-  let reconnectNumber: number | null = 0
+	let reconnectTimeout: NodeJS.Timeout | null;
+	let reconnectNumber: number | null = 0;
 
-  const open = (ws: WebSocket, config: WebSocketConfig, callback: Function) => {
-    const { reconnectTime, maxReconnectNumber = 1 } = config
-    const isOpen = getState(ws, 'OPEN')
+	const open = (ws: WebSocket, config: WebSocketConfig, callback: Function) => {
+		const { reconnectTime, maxReconnectNumber = 1 } = config;
+		const isOpen = getState(ws, "OPEN");
 
-    if (isOpen || (reconnectTimeout && reconnectNumber! >= maxReconnectNumber)) {
-      clearTimeout(reconnectTimeout as NodeJS.Timeout)
-      reconnectTimeout = null
-      reconnectNumber = null
-      config.reconnect = false
-      return
-    }
+		if (
+			isOpen ||
+			(reconnectTimeout && reconnectNumber! >= maxReconnectNumber)
+		) {
+			clearTimeout(reconnectTimeout as NodeJS.Timeout);
+			reconnectTimeout = null;
+			reconnectNumber = null;
+			config.reconnect = false;
+			return;
+		}
 
-    reconnectTimeout = setTimeout(() => {
-      reconnectNumber! += 1
-      callback()
-    }, reconnectTime)
-  }
+		reconnectTimeout = setTimeout(() => {
+			reconnectNumber! += 1;
+			callback();
+		}, reconnectTime);
+	};
 
-  return open
+	return open;
 }
 
-
 function heartbeat(ws: WebSocket, config: WebSocketConfig) {
-  const { heartbeatTime } = config
-  let timer: NodeJS.Timeout | null = null
+	const { heartbeatTime } = config;
+	let timer: NodeJS.Timeout | null = null;
 
-  timer = setInterval(() => {
-    const isOpen = getState(ws, 'OPEN');
-    const isConnected = getState(ws, 'CONNECTING');
+	timer = setInterval(() => {
+		const isOpen = getState(ws, "OPEN");
+		const isConnected = getState(ws, "CONNECTING");
 
-    if (isOpen) {
-      useSendMessage(ws, {
-        type: 'heartbeat',
-        timestamp: Date.now()
-      });
-    } else if (!isConnected) {
-      const open = reconnectWebSocket();
+		if (isOpen) {
+			useSendMessage(ws, {
+				type: "heartbeat",
+				timestamp: Date.now(),
+			});
+		} else if (!isConnected) {
+			const open = reconnectWebSocket();
 
-      open(ws, config, () => {
-        timer && clearInterval(timer);
-        heartbeat(ws, config)
-      });
-    }
-  }, heartbeatTime)
+			open(ws, config, () => {
+				timer && clearInterval(timer);
+				heartbeat(ws, config);
+			});
+		}
+	}, heartbeatTime);
 }
 
 export function useSendMessage(ws: WebSocket, data: any) {
-    const isOpen = getState(ws, 'OPEN')
-    if (isOpen) ws.send(JSON.stringify(data))
+	const isOpen = getState(ws, "OPEN");
+	if (isOpen) ws.send(JSON.stringify(data));
 }
 
 export function useOnMessage(ws: WebSocket, callback: Function) {
-  try {
-    ws.onmessage = (e: MessageEvent<any>) => {
-      const data = JSON.parse(e.data)
+	try {
+		ws.onmessage = (e: MessageEvent<any>) => {
+			const data = JSON.parse(e.data);
 
-      callback(data, e)
-    }
-  } catch (error) {
-    console.error(error)
-  }
+			callback(data, e);
+		};
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 export function useClose(ws: WebSocket, code?: number, message?: string) {
-  code = code || 1000
-  message = message || 'connection closed by client'
-  ws.close(code, message)
+	code = code || 1000;
+	message = message || "connection closed by client";
+	ws.close(code, message);
 }
