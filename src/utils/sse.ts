12d411b5cb567bf16d39b8bace69b3805c32a516
kebi <kanglu-email@qq.com>
2026-01-08 00:19:23 +0800

    feat: 新增SSE html demo

--- src/utils/sse.ts
+++ src/utils/sse.ts
@@ -1,4 +1,7 @@
 
+interface StreamRequestConfig extends RequestInit {
+    parseResponse?: boolean
+}
 interface StreamRequestParams {
     [key: string]: any
 }
@@ -8,17 +11,21 @@ export class StreamRequest {
     private options: StreamRequestParams
     private messageCallback?: <T>(data: T) => void
     private errorCallback?: <T>(data: T) => void
+    private parseResponse?: boolean
 
-    constructor(url: string, options?: any) {
+    constructor(url: string, config?: StreamRequestConfig) {
+        const { parseResponse, ...options } = config as StreamRequestConfig
+
         this.url = url
         this.options = options || {}
+        this.parseResponse = parseResponse
     }
 
     async send(params: StreamRequestParams) {
-        const { url, options, messageCallback, errorCallback } = this
+        const { url, options, parseResponse } = this
 
         try {
-            const res = await fetch(url, {
+            const res = await window.fetch(url, {
                 ...options,
                 body: JSON.stringify(params),
             })
@@ -35,11 +42,13 @@ export class StreamRequest {
                 const newData = data.split('\n')
 
                 newData.forEach((message: string) => {
-                    if (message.trim()) messageCallback?.(JSON.parse(message || '{}'))
+                    if (message.trim()) {
+                        this.messageCallback?.(parseResponse ? JSON.parse(message || '{}') : message)
+                    }
                 })
             }
         } catch (error) {
-            errorCallback?.(error)
+            this.errorCallback?.(error)
         }
     }
 
